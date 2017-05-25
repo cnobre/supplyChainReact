b95e68d9e4cb228469c538974a649daaff36933a
cnobre <carolinanobre@gmail.com>
2017-05-25 13:09:26 -0400

    minor layout changes to the whole App

--- src/App.js
+++ src/App.js
@@ -3,20 +3,26 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 // import MyAwesomeReactComponent from './MyAwesomeReactComponent';
 import TableComponent from './TableComponent';
 import SimpleMap from './GoogleMap';
+import GraphApp from './Graph'
+
+import tableData from './companyInfo.json';
 
 const App = () => (
 
   <MuiThemeProvider>
-  <div>
-    
-    <div style={{width: '80%', height: '400px', margin:'auto'}}>
+  <div style={{width: '80%',  margin:'auto'}}>
+ 
+  <div style={{width: '50%', height: '400px', margin:'auto', display:'inline-block'}}>
+    <GraphApp />
+  </div>
+
+    <div style={{width: '50%', height: '400px', margin:'auto', display:'inline-block'}}>
     <SimpleMap/>
   </div>
-  <TableComponent/>
+
+  <TableComponent data={tableData}/>
     </div>
    </MuiThemeProvider>
-  
-
 );
 
 export default App;
