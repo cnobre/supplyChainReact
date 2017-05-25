7b78b003ff3d5ec9e44ddaff9ff9e41539899c46
cnobre <carolinanobre@gmail.com>
2017-05-25 14:39:02 -0400

    added paper component around map and node-link diagram. improved layout and size of divs

--- src/App.js
+++ src/App.js
@@ -5,20 +5,34 @@ import TableComponent from './TableComponent';
 import SimpleMap from './GoogleMap';
 import GraphApp from './Graph'
 
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+
+
+
 import tableData from './companyInfo.json';
 
 const App = () => (
 
   <MuiThemeProvider>
   <div style={{width: '80%',  margin:'auto'}}>
- 
-  <div style={{width: '50%', height: '400px', margin:'auto', display:'inline-block'}}>
-    <GraphApp />
+
+
+ <Paper style={{
+  margin: '16px 0px 16px 0px',
+}}> 
+  <div style={{width: '50%', height: '400px', margin:'auto', display:'inline-block', 'overflow':'visible'}}>
+
+  	<div style={{width: '200%', height: '400px', margin:'auto', display:'inline-block'}}>
+    <SimpleMap/>
+    </div>
   </div>
 
-    <div style={{width: '50%', height: '400px', margin:'auto', display:'inline-block'}}>
-    <SimpleMap/>
+  <div style={{width: '50%', height: '400px', margin:'auto', display:'inline-block'}}>
+    <GraphApp />   
   </div>
+
+  </Paper>
 
   <TableComponent data={tableData}/>
     </div>
